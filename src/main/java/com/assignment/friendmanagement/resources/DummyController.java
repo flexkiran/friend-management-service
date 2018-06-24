aac0e4e5861881cc3d6552cc742923b9e6d3ad23
Kiran <Kiran@Mdaqs-MacBook-Pro.local>
2018-06-24 14:27:51 +0800

    Using swagger io for api documentation

--- src/main/java/com/assignment/friendmanagement/resources/DummyController.java
+++ src/main/java/com/assignment/friendmanagement/resources/DummyController.java
@@ -1,5 +1,8 @@
 package com.assignment.friendmanagement.resources;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DummyController {
 
+    @ApiOperation(value = "Returns Api Version")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(code = 100, message = "100 is the message"),
+                    @ApiResponse(code = 200, message = "Successful Version retrieval")
+            }
+    )
     @RequestMapping(method = {RequestMethod.GET}, value = {"/version"})
     public String getVersion() {
         return "1.0";
